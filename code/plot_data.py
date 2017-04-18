b0f49aaa0e38dbe23347605b8f27f22d0811cb7e
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-18 20:08:07 +1200

    labels

--- code/plot_data.py
+++ code/plot_data.py
@@ -6,10 +6,11 @@ data = np.loadtxt("j1131.txt")
 def plot_subset(image):
     subset = data[:,3] == image
     plt.errorbar(data[subset,0], data[subset,1], yerr=data[subset,2],
-                 fmt="o", markersize=3, alpha=0.3)
+                 fmt="o", markersize=3, alpha=0.3,
+                 label="Image {image}".format(image=image))
 
 for image in range(0, 4):
     plot_subset(image)
-
+plt.legend()
 plt.show()
 
