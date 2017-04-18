4fe7c0fc940b03dc3364d014394d157ae231ea62
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-18 12:23:24 +1200

    Plot data nicely

---
+++ code/plot_data.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.loadtxt("j1131.txt")
+
+def plot_subset(image):
+    subset = data[:,3] == image
+    plt.errorbar(data[subset,0], data[subset,1], yerr=data[subset,2],
+                 fmt="o", markersize=3, alpha=0.3)
+
+for image in range(0, 4):
+    plot_subset(image)
+
+plt.show()
+
